bcbc1725da88c597d630b4080fb948aaafe8b70f
sondosabdalaal12 <engsondosabdalaal@outlook.com>
2026-09-22 21:09:57 +0300

    secret key

--- src/main/java/com/dazzle/asklepios/config/SecurityConfiguration.java
+++ src/main/java/com/dazzle/asklepios/config/SecurityConfiguration.java
@@ -67,6 +67,8 @@ public class SecurityConfiguration {
                 .pathMatchers(HttpMethod.POST, "/api/account/create-password/finish").permitAll()
                 .pathMatchers(HttpMethod.GET,  "/api/patient/patient-satisfaction-survey/submit").permitAll()
                 .pathMatchers(HttpMethod.POST, "/api/patient/patient-satisfaction-survey/submit").permitAll()
+                .pathMatchers(HttpMethod.POST, "/api/patient/patient-portal/request-otp").permitAll()
+                .pathMatchers(HttpMethod.POST, "/api/patient/patient-portal/verify-login-otp").permitAll()
                 .pathMatchers(HttpMethod.POST , "/api/hl7-events").permitAll()
                 .pathMatchers("/api/authenticate").permitAll()
                 .pathMatchers("/api/register").permitAll()
